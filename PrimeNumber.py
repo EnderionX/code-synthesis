4e68fed35088c6fcd5b0602ab89b987ef990208f
Soutrik Banerjee <150548253+EnderionX@users.noreply.github.com>
2026-03-18 22:12:18 +0530

    PrimeNumber.py added;

---
+++ PrimeNumber.py
@@ -0,0 +1,14 @@
+#This program checks whether a number is prime or not;
+
+print("Prime Number Checker")
+num = int(input("Enter a number to check if it's prime or not:\n"))
+
+if num>1:
+    for i in range(2, num):
+        if (num%i)==0:
+            print(num, "is a composite number.")
+            break
+    else:
+        print(num, "is a prime number.")
+else:
+    print(num, "is not a prime number")
