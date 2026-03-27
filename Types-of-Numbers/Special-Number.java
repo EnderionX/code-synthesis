14975c86d22c43dfffc9104c44ce6912fe7b38e9
Soutrik Banerjee <150548253+EnderionX@users.noreply.github.com>
2026-03-27 12:30:52 +0530

    Update main method signature to include args

--- Types-of-Numbers/Special-Number.java
+++ Types-of-Numbers/Special-Number.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Special_Number
 {
-    public static void main()
+    public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number to check if it is a special number or not:");
